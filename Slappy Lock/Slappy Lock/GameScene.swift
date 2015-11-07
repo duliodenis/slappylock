d70e4e25786a55cdd010c8f7d92ef2dcc1ff84b1
Dulio Denis <dulio.denis@yahoo.com>
2015-11-07 18:35:58 -0500

    Added the circle with a UIBezierPath

--- Slappy Lock/Slappy Lock/GameScene.swift
+++ Slappy Lock/Slappy Lock/GameScene.swift
@@ -9,17 +9,32 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var lock = SKShapeNode()
+    var path = UIBezierPath()
+    
+    let zeroAngle: CGFloat = 0.0
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-
+        layoutGame()
+    }
+    
+    func layoutGame() {
+        backgroundColor = SKColor.whiteColor()
+        
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width/2, y: self.frame.height/2), radius: 120, startAngle: zeroAngle, endAngle: zeroAngle + CGFloat(M_PI * 2), clockwise: true)
+        
+        lock = SKShapeNode(path: path.CGPath)
+        lock.strokeColor = SKColor.grayColor()
+        lock.lineWidth = 40.0
+        self.addChild(lock)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
 
     }
    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        
     }
 }
